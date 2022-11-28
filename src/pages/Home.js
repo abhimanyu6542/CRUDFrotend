2c8e3947466eb6c46773246449ad528d57094b46
Abhimanyu Debata <107947041+abhimanyu6542@users.noreply.github.com>
2022-11-29 00:54:31 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+
+    const [ data, setData ] = useState([]);
+
+    const getUsers = async () => {
+        const response = await axios.get(`http://localhost:3200/users`);
+        if(response.status = 200) {
+            setData(response.data);
+        }
+    };
+
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    const deleteUser = async (id) => {
+        console.log("inside delete user: id is", id);
+        const response = await axios.delete(`http://localhost:3200/user/${id}`);
+        if(response.status === 200) {
+            alert(`user deleted successfully`);
+        }
+        getUsers();
+    };
+
+    console.log(data);
+
+    return (
+        <div style={{ marginTop: "100px" }}>
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ textAlign: 'center' }}>No.</th>
+                        <th style={{ textAlign: 'center' }}>Name</th>
+                        <th style={{ textAlign: 'center' }}>Email</th>
+                        <th style={{ textAlign: 'center' }}>Contact</th>
+                        <th style={{ textAlign: 'center' }}>Action</th>
+                    </tr>    
+                </thead>  
+                <tbody>
+                    {
+                        data && data.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <th scope='row'>{index+1}</th>
+                                    <td>{item.name}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.contact}</td>
+                                    <td>
+                                        <Link to={`/update/${item._id}`}>
+                                            <button>Edit</button>
+                                        </Link>
+                                        <button onClick={() => deleteUser(item._id)}>Delete</button>
+                                        <Link to={`/view/${item._id}`}>
+                                            <button >View</button>
+                                        </Link>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }    
+                </tbody>  
+            </table>            
+        </div>    
+    );
+};
+
+export default Home;
